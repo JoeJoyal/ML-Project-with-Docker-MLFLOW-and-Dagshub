317b0a0a24aa5dfd561fb821a48bfa9c07d80ad2
github_username <email_address>
2024-11-22 22:08:13 +0530

    utils folder added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 from src.logger import logging
-from src.exception.exception import customexception
+from src.exception import customexception
 
 from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 
@@ -19,6 +19,29 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
+        raise customexception(e,sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            # Predict Testing data
+            y_test_pred = model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.info('Exception occured during model training')
+
         raise customexception(e,sys)
     
 def load_object(file_path):
